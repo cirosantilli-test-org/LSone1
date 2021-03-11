ac47fb6a32fc8fb8f97414ff2da25a2ee58b3f16
Bruce <1677366451@qq.com>
2021-03-11 23:53:29 +0800

    error message

---
+++ error/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Error handler</title>
+    <style>
+    div {
+      border-radius: 5px;
+      background-color: #f2f2f2;
+      padding: 20px;
+    }
+
+    h2 {
+      text-align: center;
+    }
+
+    input[type=text] {
+      width: 100%;
+      padding: 12px 20px;
+      margin: 8px 0;
+      display: inline-block;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      box-sizing: border-box;
+    }
+
+    button {
+      font-size: 16px;
+      width: 100%;
+      background-color: #4CAF50;
+      color: white;
+      padding: 14px 20px;
+      margin: 8px 0;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+    }
+
+    button:hover {
+      background-color: #45a049;
+    }
+
+    .error {
+      color: red;
+    }
+
+    .success {
+      color: green;
+    }
+    </style>
+  </head>
+  <body>
+  <div>
+    <h2>Sign up</h2>
+    <form action="signup.inc.php" method="post">
+      <input type="text" name="first" placeholder="Firstname">
+      <input type="text" name="last" placeholder="Lastname">
+      <input type="text" name="email" placeholder="Email">
+      <input type="text" name="uid" placeholder="Username">
+      <input type="text" name="pwd" placeholder="Password">
+      <button type="submit" name="submit">Sign up</button>
+    </form>
+  </div>
+
+  <?php
+  $fullUrl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+  if (strpos($fullUrl, "signup=empty") == true) {
+    echo "<p class='error'>You did not fill in a fields!</p>";
+    exit();
+  }
+  elseif (strpos($fullUrl, "signup=char") == true) {
+    echo "<p class='error'>You used invalid characters!</p>";
+    exit();
+  }
+  elseif (strpos($fullUrl, "signup=invalidemail") == true) {
+    echo "<p class='error'>You used invalid email!</p>";
+    exit();
+  }
+  elseif (strpos($fullUrl, "signup=success") == true) {
+    echo "<p class='success'>You have been signed up!</p>";
+    exit();
+  }
+   ?>
+  </body>
+</html>
