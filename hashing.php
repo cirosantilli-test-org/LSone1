614febc11e7401b70a0e37f3d0d1ed620ba3ab36
Bruce <1677366451@qq.com>
2021-03-14 22:36:08 +0800

    hashing and dehashing data

---
+++ hashing.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Hashing and de-hashing</title>
+  </head>
+  <body>
+
+    <?php
+    //Hashing
+    /*
+    echo "test123";
+    echo "<br>";
+    echo password_hash("test123", PASSWORD_DEFAULT);
+    */
+
+    /*
+    //De-hashing
+    $input = "test123";
+    $hashedPwdInDb = password_hash("test123", PASSWORD_DEFAULT);
+
+    echo password_verify($input, $hashedPwdInDb);
+    */
+    
+     ?>
+
+  </body>
+</html>
