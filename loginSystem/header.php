65ff7e38f0bfb52b260673cbc6850fd076a00e23
Bruce <1677366451@qq.com>
2021-03-15 22:21:29 +0800

    login system

---
+++ loginSystem/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link href="css/styles.css" rel="stylesheet">
+</head>
+<body>
+
+<header>
+  <nav>
+    <a href="#">
+      <img src="favicon.ico" alt="logo">
+    </a>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="#">Portfolio</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+    <div>
+      <h2>Sign up</h2>
+      <form action="signup.inc.php" method="post">
+        <input type="text" name="" placeholder="">
+        <input type="text" name="" placeholder="">
+        <input type="text" name="" placeholder="">
+        <input type="text" name="" placeholder="">
+        <button type="submit" name="submit">Submit</button>
+      </form>
+    </div>
+  </nav>
+</header>
