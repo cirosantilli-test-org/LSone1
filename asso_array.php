d6b653f4a08eed3595ebd5fa8e1bf80f621c4e87
Bruce <1677366451@qq.com>
2021-03-29 11:24:23 +0800

    associative arrays

---
+++ asso_array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    $data["firstname"] = "Bruce";
+    $data["lastname"] = "Zhang";
+    $data["age"] = 22;
+    print_r($data);
+    ?>
+
+  </body>
+</html>
