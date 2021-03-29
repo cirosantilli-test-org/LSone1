a1bf3eaad874835451ff2d0001523542538e7276
Bruce <1677366451@qq.com>
2021-03-29 11:31:17 +0800

    multidimensional arrays

---
+++ mult_array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    $data = array(array(1, 2, 3),
+    "Bruce",
+    "Zhang"
+  );
+  //print_r($data);
+  echo $data[0][0]; 
+  ?>
+  </body>
+</html>
